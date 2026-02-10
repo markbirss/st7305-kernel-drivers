54fb9389d7dc1abf68f939957a9d6a72dab26cae
Mark Trevor Birss <markbirss@gmail.com>
2026-02-10 15:04:58 +0200

    Update st7305.c

--- st7305.c
+++ st7305.c
@@ -170,6 +170,7 @@ static void st7305_pipe_enable(struct drm_simple_display_pipe *pipe,
 	mipi_dbi_command(dbi, 0xD0, 0xFF); // Auto power down
 	mipi_dbi_command(dbi, 0x38); // High Power Mode on
 	mipi_dbi_command(dbi, 0xBB, 0x4F); // Enable Clear RAM
+	msleep(100);
 
 	if (st7305->te)
 		mipi_dbi_command(dbi, 0x35, 0x00); // 0b00: TE v-blanking mode
