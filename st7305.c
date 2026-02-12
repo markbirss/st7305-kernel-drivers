361157c659fd90b9da8593faa092327c267cd1ed
Mark Trevor Birss <markbirss@gmail.com>
2026-02-12 09:24:34 +0200

    Update st7305.c

--- st7305.c
+++ st7305.c
@@ -323,6 +323,8 @@ static void st7305_fb_dirty(struct drm_framebuffer *fb, struct drm_rect *rect)
 	if (!drm_dev_enter(fb->dev, &idx))
 		return;
 
+	//msleep(120);	// Bugfix for kernel crash
+
 	DRM_DEBUG_KMS("Flushing [FB:%d] " DRM_RECT_FMT "\n", fb->base.id,
 		      DRM_RECT_ARG(rect));
 
@@ -359,6 +361,8 @@ static void st7305_pipe_update(struct drm_simple_display_pipe *pipe,
 	struct drm_framebuffer *fb = state->fb;
 	struct drm_rect rect;
 	
+	//msleep(20);	// Bugfix for kernel crash
+		
 	if (!pipe->crtc.state->active)
 		return;
 
@@ -369,9 +373,9 @@ static void st7305_pipe_update(struct drm_simple_display_pipe *pipe,
 		rect.y2 = fb->height;
 		st7305_fb_dirty(state->fb, &rect);
 	} else {
-		if (drm_atomic_helper_damage_merged(old_state, state, &rect)) 
-		{
+		if (drm_atomic_helper_damage_merged(old_state, state, &rect)) {
 			st7305_fb_dirty(state->fb, &rect); 
+			//msleep(20);		// Bugfix for kernel crash
 		}
 	}
 }
